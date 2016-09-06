51cfcd0be5824575fc72fae005228dea1735b1b2
Hoang Thanh Nguyen <thanhnguyen.bhtech@gmail.com>
2016-09-06 23:07:22 +0700

    Add model

---
+++ Android_HackerNews/app/src/main/java/com/innovatube/hackernews/data/model/StoryId.java
@@ -0,0 +1,18 @@
+package com.innovatube.hackernews.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Thanh on 05/09/2016.
+ */
+public class StoryId {
+    private Long storyId;
+
+    public Long getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(Long storyId) {
+        this.storyId = storyId;
+    }
+}
