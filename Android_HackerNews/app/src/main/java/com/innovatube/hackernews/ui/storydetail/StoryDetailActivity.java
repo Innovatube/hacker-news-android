d66238080ef519985c360088e998854c7c2d0965
Hoang Thanh Nguyen <thanhnguyen.bhtech@gmail.com>
2016-09-14 10:33:11 +0700

    Fix issue : [Story Detail] Missing share button in story detail

--- Android_HackerNews/app/src/main/java/com/innovatube/hackernews/ui/storydetail/StoryDetailActivity.java
+++ Android_HackerNews/app/src/main/java/com/innovatube/hackernews/ui/storydetail/StoryDetailActivity.java
@@ -1,19 +1,27 @@
 package com.innovatube.hackernews.ui.storydetail;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 import com.innovatube.hackernews.R;
 import com.innovatube.hackernews.ui.base.BaseActivityWithDialog;
 import com.innovatube.hackernews.ui.topstory.TopStoriesActivity;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class StoryDetailActivity extends BaseActivityWithDialog implements StoryDetailViewInterface {
     @BindView(R.id.webview)
@@ -25,7 +33,6 @@ public class StoryDetailActivity extends BaseActivityWithDialog implements Story
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_story_detail);
         ButterKnife.bind(this);
         getActivityComponent().inject(this);
@@ -54,5 +61,31 @@ public class StoryDetailActivity extends BaseActivityWithDialog implements Story
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
+    }
+
+    @OnClick(R.id.btnShareFacebook)
+    public void OnButtonShareFacebookClick() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, presenter.getUrl());
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+        boolean facebookInstalled = false;
+        for (final ResolveInfo app : activityList) {
+            if ((app.activityInfo.name).contains("facebook")) {
+                facebookInstalled = true;
+                final ActivityInfo activity = app.activityInfo;
+                final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+                shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                shareIntent.setComponent(name);
+                startActivityForResult(shareIntent, 0);
+                break;
+            }
+        }
+        if (!facebookInstalled) {
+            showAlertDialog(getString(R.string.facebook_not_installed));
+        }
     }
 }
